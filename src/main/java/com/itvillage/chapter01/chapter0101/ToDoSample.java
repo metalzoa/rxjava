00d3300f0d0917435467882cd2ef910aace4d68a
metalzoa <metalzoa@naver.com>
2020-08-28 16:46:05 +0900

    주석 추가

--- src/main/java/com/itvillage/chapter01/chapter0101/ToDoSample.java
+++ src/main/java/com/itvillage/chapter01/chapter0101/ToDoSample.java
@@ -8,9 +8,9 @@ public class ToDoSample {
         Observable.just(100, 200, 300, 400, 500) // data 발행
                 .doOnNext(data -> System.out.println(getThreadName() + " : " + "#doOnNext() : " + data)) // data 발행
                 .subscribeOn(Schedulers.io()) // main 쓰래드 아닌 다른 쓰래드 에서 실행 하게됨, 이 쓰래드가 실행 되기 전에 종료 되버리면 아무것도 실행 안되기 떄문에 하단에 Thread.sleep 가 존재함
-                // RxCachedThread 는 위에 subscribeOn 실행시 나타남
+                // RxCachedThread 는 위에 subscribeOn 실행시 나타남, 데이터 발생/흐름 쓰래드 결정
                 .observeOn(Schedulers.computation())
-                // 데이터 발행과 구독 쓰래드가 분리됨
+                // 데이터 발행과 구독 쓰래드가 분리됨, 발행된 데이터 처리 하는 쓰래드 지정
                 // 발행 쓰래드 : RxCachedThreadPool
                 // 구독 쓰래드 : RxComputatioinThreadPool
                 .filter(number -> number > 300)
